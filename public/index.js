d7c9f6a57ba8005a9a72d3fc3a790a4acbdfe98e
Jake Marsh <JMarsh2201@gmail.com>
2018-09-29 11:17:17 -0600

    add countries to backend

--- public/index.js
+++ public/index.js
@@ -1,10 +1,23 @@
 $(document).ready(() => {
-  $("#submit").click(function(){
+
+  // not sure what this is doing. Not sure why first $
+  // is purple, not orange.
+  let $country = $('#country');
+
+
+  $("#submit").click(function() {
+    let country = {
+    name: $country.val()
+    }
+
+    console.log(country)
+
     $.ajax({
-      url: "/api/country/",
-      success: (res) => {
-        $.post( { 'country': 'Egypt' } );
-        console.log(res);
+      type: 'POST',
+      url: '/api/country',
+      data: country,
+      success: function(newCountry) {
+        console.log(newCountry)
       }
     })
   })
