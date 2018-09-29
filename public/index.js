87649337adc3254b13636c8bac367fe555094b32
Jake Marsh <JMarsh2201@gmail.com>
2018-09-29 11:33:29 -0600

    added list of all countries

--- public/index.js
+++ public/index.js
@@ -3,6 +3,18 @@ $(document).ready(() => {
   // not sure what this is doing. Not sure why first $
   // is purple, not orange.
   let $country = $('#country');
+  let $countries = $('#listCountries')
+
+  $.ajax({
+    type: 'GET',
+    url: 'api/country',
+    success: function(countries) {
+      $.each(countries, function(i, name)  {
+        $countries.append('<li>country: ' + name.name + '</li>')
+      })
+    }
+
+  })
 
 
   $("#submit").click(function() {
