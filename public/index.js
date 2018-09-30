d9743053231e449d97b1adcea2343fb6bccfb200
Jake Marsh <JMarsh2201@gmail.com>
2018-09-30 10:20:52 -0600

    country update WIP

--- public/index.js
+++ public/index.js
@@ -1,36 +1,52 @@
 $(document).ready(() => {
 
-  // not sure what this is doing. Not sure why first $
-  // is purple, not orange.
   let $country = $('#country');
-  let $countries = $('#listCountries')
+  let $countries = $('#dropDown')
 
-  $.ajax({
-    type: 'GET',
-    url: 'api/country',
-    success: function(countries) {
-      $.each(countries, function(i, name)  {
-        $countries.append('<li>country: ' + name.name + '</li>')
-      })
-    }
+  let $newCountry = $('#NewCountry')
+  let $AddNew = $('#AddNewCountry')
 
-  })
-
-
-  $("#submit").click(function() {
-    let country = {
-    name: $country.val()
-    }
-
-    console.log(country)
-
+  // populates <ul> of countries that exist in the back end
     $.ajax({
-      type: 'POST',
-      url: '/api/country',
-      data: country,
-      success: function(newCountry) {
-        console.log(newCountry)
+      type: 'GET',
+      url: 'api/country',
+      success: function(countries) {
+        $.each(countries, function(i, name)  {
+          $countries.append('<option>' + this.name + '</option>')
+        })
+      },
+      complete: function() {
+        $('#update').click(function () {
+          if ($countries.val() === "Add New Country") {
+            $newCountry.empty()
+            $newCountry.append('<input type="" id="AddNewCountry" placeholder="add country">')
+            $newCountry.append('<input type="" id="AddNewRegion" placeholder="add region">')
+            $newCountry.append('<button type="button" id="submit">Submit</button>')
+
+          } else {
+            $newCountry.empty()
+            $newCountry.append('<input type="" id="AddNewRegion" placeholder="add region">')
+            $newCountry.append('<button type="button" id="submit">Submit</button>')
+          }
+        })
       }
     })
-  })
+
+  // upon click, posts country object to back end
+    // page reload will show newly added country to the <ul>
+  // $("#submit").click(function() {
+  //   let country = {
+  //     name: $country.val()
+  //   }
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: '/api/country',
+  //     data: country,
+  //     success: function(newCountry) {
+  //       console.log(newCountry)
+  //     }
+  //   })
+  // })
+
+
 })
