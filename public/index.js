fc84f789a0c05a907d89c8c438187817a888478d
Jake Marsh <JMarsh2201@gmail.com>
2018-09-30 20:15:34 -0600

    I might have added regions

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
 
   let json;
+  let countryID;
   let $countries = $('#dropDown')
   let $newCountry = $('#newCountry')
-
 
   // populates <ul> of countries that exist in the back end
     $.ajax({
@@ -17,7 +17,12 @@ $(document).ready(() => {
         // adds or removes input fields based on dropDown selection
         $('#dropDown').change(function () {
           json = countries
-
+          for (var i = 0; i < json.length; i++) {
+            if (json[i]['name'] === $countries.val()) {
+              countryID = json[i]['_id']
+            }
+          }
+          console.log(countryID)
           if ($countries.val() === "Add New Country") {
             $newCountry.empty()
             $('#subButton').empty()
@@ -33,23 +38,49 @@ $(document).ready(() => {
             $newCountry.append('<input type="" id="AddNewRegion" placeholder="add region">')
             $('#subButton').append('<button type="button" id="submit">Submit</button>')
           }
-          console.log(countries[0]['_id'])
+          // console.log(countries[0]['_id'])
         })
       }
     })
     // Posts data upon clicking submit button
     $('#subButton').on('click', $('#submit'), function() {
       let nameField = $('#AddNewCountry').val()
-      if (nameField !== undefined && nameField !== "") {
+      let regField = $('#AddNewRegion').val()
+      if (nameField !== undefined && nameField !== '') {
         let country = {
           name: nameField
         }
+        $('#errors').empty()
         $.ajax({
           type: 'POST',
           url: 'api/country',
           data: country,
           success: function(res) {
             console.log(res)
+            }
+          })
+        }
+        else if (nameField === "") {
+          $('#errors').empty().append('<h4>please enter a country, first.</h4>')
+        }
+
+// problem child: does not push name and country to
+        else if (nameField == undefined && regField.length > 0) {
+          let region = {
+            name: regField
+          }
+
+          $.ajax({
+            type: 'POST',
+            url: `api/country/${countryID}/regions`,
+            data: region,
+            success: function(res) {
+              console.log(res)
+              for (var i = 0; i < json.length; i++) {
+                if (json[i]['_id'] === countryID) {
+                  json[i]['regions'].push(region)
+                }
+              }
             }
           })
         }
